18f30344edc9ea4dc519bf90b06877993daa23b3
nezakrzan <nezza.krzzan@gmail.com>
2021-01-10 18:29:20 +0100

    Napredna anliza

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -8,13 +8,16 @@ napoved_slo <- min_place %>%
   filter(Drzava=="Slovenia")
 odstopanje <- lm(data = napoved_slo, Placa ~ as.numeric(Leto))
 g <- data.frame(Leto=seq(2016, 2024, 1))
-napoved <- mutate(g, Placa=predict(odstopanje, g))
+napoved <- g %>% mutate(Placa=predict(odstopanje, .))
 
 graf_odstopanje <- ggplot(napoved_slo, aes(x=Leto, y=Placa)) + 
-  geom_point() + geom_smooth(method=lm, fullrange = TRUE, color = 'red') + 
-  geom_point(data=odstopanje, aes(x= Leto, y= Placa), color='dark green', size=2) +
+  geom_point(shape=1) + 
+  geom_smooth(method=lm, formula = x~y, color = 'red') + 
+  geom_point(data=napoved, aes(x=Leto, y=Placa), color='dark green', size=2) +
   ggtitle("Napoved nadaljne rasti minimalne bruto plače v Sloveniji") +
   ylab("Plača")
 
 #Realna min. plača v 2018:  842,79€
 #Realna min. plača v 2019: 886,63€
+
+  
