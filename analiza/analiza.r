cff9bea6e5779113ef729d1e125f5ebdf0a87d1e
nezakrzan <nezza.krzzan@gmail.com>
2021-01-10 10:33:52 +0100

    Napredna analiza

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -3,54 +3,18 @@
 require(ggplot2)
 require(dplyr)
 
-#Krizi(2008,2020)
-meseci <- c("12", "11","10","09","08","07","06","05","04","03","02","01",
-            "12", "11","10","09","08","07","06","05","04","03","02","01",
-            "12", "11","10","09","08","07","06","05","04","03","02","01")
-leto <- c("2009","2009","2009","2009","2009","2009","2009","2009","2009","2009","2009","2009",
-          "2008","2008","2008","2008","2008","2008","2008","2008","2008","2008","2008","2008",
-          "2007","2007","2007","2007","2007","2007","2007","2007","2007","2007","2007","2007")
-kriza2008_meseci <- data.frame(leto, meseci, kriza2008$placa)
-kriza_leto2009 <- kriza2008_meseci %>%
-  filter(leto == "2009")
-kriza_leto2008 <- kriza2008_meseci %>%
-  filter(leto == "2008")
-kriza_leto2007 <- kriza2008_meseci %>%
-  filter(leto == "2007")
+#NAPOVED RASTI BRUTO MINIMALNE PLAČE V SLOVENIJI
+napoved_slo <- min_place %>%
+  filter(Drzava=="Slovenia")
+odstopanje <- lm(data = napoved_slo, Placa ~ Leto)
+g <- data.frame(Leto=seq(2016, 2024, 1))
+napoved <- mutate(g, Placa=predict(odstopanje, g))
 
-#Kriza leta 2008
-ggplot(kriza_leto2007, mapping=aes(x=meseci, y=kriza2008.placa)) +
-  geom_point(color="red", size=2) +
-  geom_point(kriza_leto2008,mapping=aes(x=meseci, y=kriza2008.placa), color="blue", size=2) +
-  geom_point(kriza_leto2009,mapping=aes(x=meseci, y=kriza2008.placa), color="yellow", size=2) +
-  labs(title="Primerjava plače v letih 2007, 2008, 2009") +
-  ylab("Višina plače(€)") +
-  xlab("Meseci")
+graf_odstopanje <- ggplot(napoved_slo, aes(x=Leto, y=Placa)) + 
+  geom_point() + geom_smooth(method=lm, fullrange = TRUE, color = 'red') + 
+  geom_point(data=odstopanje, aes(x=Leto, y=Placa), color='dark green', size=2) +
+  ggtitle("Napoved nadaljne rasti minimalne bruto plače v Sloveniji") +
+  xlab("Leto")+
+  ylab("Plača")
 
-#Primerjava leta 2007 z letom 2019
-meseci <- c("09","08","07","06","05","04","03","02","01",
-            "12", "11","10","09","08","07","06","05","04","03","02","01")
-leto <- c("2020","2020","2020","2020","2020","2020","2020","2020","2020",
-          "2019","2019","2019","2019","2019","2019","2019","2019","2019","2019","2019","2019")
-kriza2020_meseci <- data.frame(leto, meseci, kriza2020$placa)
-kriza_leto2019 <- kriza2020_meseci %>%
-  filter(leto == "2019")
-
-ggplot(kriza_leto2007, mapping=aes(x=meseci, y=kriza2008.placa)) +
-  geom_point(color="red", size=2) +
-  geom_point(kriza_leto2019, mapping=aes(x=meseci, y=kriza2020.placa), color="blue", size=2) +
-  labs(title="Primerjava plače v letih pred krizo(2007,2019)") +
-  ylab("Višina plače(€)") +
-  xlab("Meseci")
-
-#Primerjava leta 2008 in 2020(do meseca 09)
-kriza_leto2020 <- kriza2020_meseci %>%
-  filter(leto == "2020")
-
-ggplot(kriza_leto2008, mapping=aes(x=meseci, y=kriza2008.placa)) +
-  geom_point(color="red", size=2) +
-  geom_point(kriza_leto2020, mapping=aes(x=meseci, y=kriza2020.placa), color="blue", size=2) +
-  labs(title="Primerjava plače v letih 2008,2020") +
-  ylab("Višina plače(€)") +
-  xlab("Meseci")
 
