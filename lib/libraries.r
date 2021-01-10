9524dade61e6b66f848d97b661078bf4215f1331
nezakrzan <nezza.krzzan@gmail.com>
2021-01-10 09:42:40 +0100

    Posodobitev knjiznic

--- lib/libraries.r
+++ lib/libraries.r
@@ -4,8 +4,11 @@ library(gsubfn)
 library(tidyr)
 library(tmap)
 library(shiny)
+library(reshape)
+library(reshape2)
 
 options(gsubfn.engine="R")
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
+
